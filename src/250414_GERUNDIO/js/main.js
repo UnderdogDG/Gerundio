81e585d7217f311d731ce1f88a67c92739ae060d
User Office <gustavobarrera.cg@gmail.com>
2025-04-16 19:13:26 -0600

    250416 - fix

--- src/250414_GERUNDIO/js/main.js
+++ src/250414_GERUNDIO/js/main.js
@@ -1,4 +1,13 @@
 import '../sass/main.scss';
 import '../index.pug';
 
-console.log("working...");
+const menuBtn = document.getElementById('hamburguer');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.setAttribute('aria-expanded', menu.getAttribute('aria-expanded') === 'true' ? 'false' : 'true');
+    menu.setAttribute('aria-hidden', menu.getAttribute('aria-hidden') === 'true' ? 'false' : 'true');
+    menuBtn.setAttribute('aria-expanded', menu.getAttribute('aria-expanded'));
+    menuBtn.setAttribute('aria-hidden', menu.getAttribute('aria-hidden'));
+    menu.classList.toggle('active');
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,23 @@
 const path = require('path');
-const glob = require('glob');
+// const glob = require('glob');
 const projects = require('./projects');
-const imgDefs = require('./imgDefs');
+// const imgDefs = require('./imgDefs');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const PurgecssPlugin = require('purgecss-webpack-plugin');
+// const PurgecssPlugin = require('purgecss-webpack-plugin');
 
-const project = projects['250128_KM_DUNNA'];
+const project = projects['250414_GERUNDIO'];
 
 console.log(path.resolve(__dirname, `./src/projects/${project.projectFolder}${project.outputProjectFolder}`));
 
 module.exports = {
     mode: 'production',
-    entry: `/src/projects/${project.projectFolder}/js/${project.jsEntry}.js`,
+    entry: `/src/${project.projectFolder}/js/${project.jsEntry}.js`,
 
     output: {
-        path: path.resolve(__dirname, `./src/projects/${project.projectFolder}${project.outputProjectFolder}`),
+        path: path.resolve(__dirname, `./build`),
         filename: '[name].js',
         publicPath: './'
     },
@@ -88,67 +88,67 @@ module.exports = {
                     }
                 }
             },
-            {
-                test: /\.(jpg|png)$/,
-                use:{
-                    loader: 'sharp-loader',
-                    options:{
-                        name: '[name].[ext]',
-                        cacheDirextory: true,
-                        presets:{
+            // {
+            //     test: /\.(jpg|png)$/,
+            //     use:{
+            //         loader: 'sharp-loader',
+            //         options:{
+            //             name: '[name].[ext]',
+            //             cacheDirextory: true,
+            //             presets:{
                             
-                            base:{
-                                name: './img/[name].[ext]',
-                                format: (meta)=>{
-                                    /* if(meta.hasAlpha){
-                                        return{ ...imgDefs.pngBase }
-                                    }
-                                    return { ...imgDefs.jpegBase } */
-                                    return{ ...imgDefs.pngBase }
-                                },
-                                width: 1200
-                            },
+            //                 base:{
+            //                     name: './img/[name].[ext]',
+            //                     format: (meta)=>{
+            //                         /* if(meta.hasAlpha){
+            //                             return{ ...imgDefs.pngBase }
+            //                         }
+            //                         return { ...imgDefs.jpegBase } */
+            //                         return{ ...imgDefs.pngBase }
+            //                     },
+            //                     width: 1200
+            //                 },
 
-                            large:{
-                                name: './img/[name]_lg.[ext]',
-                                format: (meta)=>{
-                                    /* if(meta.hasAlpha){
-                                        return{ ...imgDefs.pngBase }
-                                    }
-                                    return { ...imgDefs.jpegBase } */
-                                    return{ ...imgDefs.pngBase }
-                                },
+            //                 large:{
+            //                     name: './img/[name]_lg.[ext]',
+            //                     format: (meta)=>{
+            //                         /* if(meta.hasAlpha){
+            //                             return{ ...imgDefs.pngBase }
+            //                         }
+            //                         return { ...imgDefs.jpegBase } */
+            //                         return{ ...imgDefs.pngBase }
+            //                     },
 
-                                width: 800
-                            },
+            //                     width: 800
+            //                 },
 
-                            medium:{
-                                name: './img/[name]_md.[ext]',
-                                format: (meta)=>{
-                                    /* if(meta.hasAlpha){
-                                        return{ ...imgDefs.pngBase }
-                                    }
-                                    return { ...imgDefs.jpegBase } */
-                                    return{ ...imgDefs.pngBase }
-                                },
-                                width: 600
-                            },
+            //                 medium:{
+            //                     name: './img/[name]_md.[ext]',
+            //                     format: (meta)=>{
+            //                         /* if(meta.hasAlpha){
+            //                             return{ ...imgDefs.pngBase }
+            //                         }
+            //                         return { ...imgDefs.jpegBase } */
+            //                         return{ ...imgDefs.pngBase }
+            //                     },
+            //                     width: 600
+            //                 },
 
-                            small:{
-                                name: './img/[name]_sm.[ext]',
-                                format: (meta)=>{
-                                    /* if(meta.hasAlpha){
-                                        return{ ...imgDefs.pngBase }
-                                    }
-                                    return { ...imgDefs.jpegBase } */
-                                    return{ ...imgDefs.pngBase }
-                                },
-                                width: 400
-                            },
-                        },
-                    }
-                }
-            },
+            //                 small:{
+            //                     name: './img/[name]_sm.[ext]',
+            //                     format: (meta)=>{
+            //                         /* if(meta.hasAlpha){
+            //                             return{ ...imgDefs.pngBase }
+            //                         }
+            //                         return { ...imgDefs.jpegBase } */
+            //                         return{ ...imgDefs.pngBase }
+            //                     },
+            //                     width: 400
+            //                 },
+            //             },
+            //         }
+            //     }
+            // },
             {
                 test: /\.(svg)$/,
                 use:{
@@ -159,7 +159,7 @@ module.exports = {
                     }
                 }
             },
-            /* {
+            {
                 test: /\.(jpg|svg|png)$/,
                 use:{
                     loader: 'file-loader',
@@ -168,7 +168,7 @@ module.exports = {
                         outputPath:'img/'
                     }
                 }
-            }, */
+            },
             
         ]
     },
@@ -176,7 +176,7 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: path.resolve('src', `projects/${project.projectFolder}/${project.pugEntry}.pug`),
+            template: path.resolve('src', `${project.projectFolder}/${project.pugEntry}.pug`),
             templateParameters: project.globals,
             /* template: path.resolve('src', 'projects/220616_L_MM/index.pug') */
             /* template: path.resolve('src', 'projects/220612_L_TE/index_v1.pug') */
